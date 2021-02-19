3e18e521a2c0d309ad022e6074c11e73b9782c7f
Helder Balbino <helderbalbino@googlemail.com>
2021-02-19 20:34:07 +0000

    Added logic Js logic Added a function to compare the user's guess to the random number

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
-let computerGuess;
+// create global variables
+let givenRandomNumber;
+let userGuesses = [];
 
+// this random number function will be called as soon as the screen loads
+// it hides part of the screen
 function init() {
-	computerGuess = Math.floor(Math.random() * 100 + 1);
+	givenRandomNumber = Math.floor(Math.random() * 100 + 1);
+	console.log(givenRandomNumber);
 	document.getElementById('newGameButton').style.display = 'none';
 	document.getElementById('gameArea').style.display = 'none';
 }
 
+// this function is linked to the easy and Hard buttons
+// this function hides the welcomeScreen and revels the gameArea
 function startGameView() {
 	document.getElementById('welcomeScreen').style.display = 'none';
 	document.getElementById('gameArea').style.display = 'block';
 }
 
+// easy button, calling the startGameView function, onclick
 function easyMode() {
 	startGameView();
 }
-
+// hard button, calling the startGameView function, onclick
 function hardMode() {
 	startGameView();
 }
 
-// const id = setInterval(() => {
-// 	console.log(Math.random());
-// }, 2000);
+// create a function that compares the user input with the given random number generated at the start of the game
+function compareGuess() {
+	// transform the user input into a number
+	const userGuess = Number(document.getElementById('inputBox').value);
+	//pushes the user input value to the array userGuesses
+	userGuesses.push(`  ${userGuess}`);
+	//displays the userGuesses array in the span
+	document.getElementById('guesses').innerHTML = userGuesses;
 
-// const printRandom = setInterval(() => {
-// 	startGameView();
-// }, 2000);
+	if (userGuess > givenRandomNumber) {
+		document.getElementById('textOutput').innerHTML = 'Your guess was too high!';
+		document.getElementById('inputBox').value = '';
+	} else if (userGuess < givenRandomNumber) {
+		document.getElementById('textOutput').innerHTML = 'Your guess was too low!';
+		document.getElementById('inputBox').value = '';
+	} else {
+		document.getElementById('textOutput').innerHTML = ' 🎊 CONGRATULATIONS you guessed the right number!!! 🎊';
+	}
+}
